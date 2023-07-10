e64ba4dee5f300d362b0b9140f79cfd7da6da558
Santhanalakshmi <104186416+santhanalakshmi21@users.noreply.github.com>
2023-07-10 10:29:04 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,14 @@
+const greet = (username) => {
+    console.log(`Hello ${username}!`);
+  };
+  
+  const processUserInput = (callback) => {
+    console.log("Inside processUserInput");
+    callback("Gautam");
+  };
+  
+  processUserInput(greet);
+  
+  processUserInput(() => {
+    console.log("Inside Callback function");
+  });
